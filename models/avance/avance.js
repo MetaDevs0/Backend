846fe9946e3fd4719f68fcef980b6741eaf50ed9
Paula Andrea Trejos <65204309+patrejos@users.noreply.github.com>
2021-11-20 21:08:29 -0500

    crear modelo de avance y usuario

---
+++ models/avance/avance.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+//import { ProjectModel } from '../proyecto/proyecto.js';
+import { UsuarioModelo } from '../usuario/usuario.js';
+
+const { Schema, model } = mongoose;
+
+const avanceSchema = new Schema({
+  fecha: {
+    type: Date,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+  observaciones: [
+    {
+      type: String,
+    },
+  ],
+//  proyecto: {
+//    type: Schema.Types.ObjectId,
+//    ref: ProjectModel,
+//    required: true,
+//  },
+  RealizadoPor: {
+    type: Schema.Types.ObjectId,
+    ref: UsuarioModelo,
+    required: true,
+  },
+});
+
+const AvanceModelo = model('Avance', avanceSchema);
+
+export { AvanceModelo };
---
+++ models/usuario/usuario.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const usuarioSchema = new Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  apellido: {
+    type: String,
+    required: true,
+  },
+  correo :{
+    type: String,
+    required: true,
+    unique: true,
+  },
+  identificacion: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  rol: {
+    type: String,
+    required: true,
+    enum: ['ESTUDIANTE', 'LIDER', 'ADMINISTRADOR'],
+  },
+  estado: {
+    type: String,
+    enum: ['PENDIENTE', 'AUTORIZADO', 'NO_AUTORIZADO'],
+    default: 'PENDIENTE',
+  },
+});
+u
+const UsuarioModelo = model('Usuario', usuarioSchema);
+
+export { UsuarioModelo };  
